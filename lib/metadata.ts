e1bb4edee53824ad6513b754da7c6594fde74228
aviralj02 <aviraljain1311@gmail.com>
2026-09-04 20:50:45 +0530

    feat: :art: redesign the og image

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -25,8 +25,8 @@ export const portfolioMetadata: Metadata = {
     images: [
       {
         url: `${PORTFOLIO_URL}/opengraph-image.jpg`,
-        width: 1200,
-        height: 630,
+        width: 2400,
+        height: 1260,
         alt: "Aviral Jain – Portfolio",
       },
     ],
